da6c4d37869201d924fe853685d299eca5eccf56
Hendrik Schaeidt <he.schaeidt@gmail.com>
2017-02-04 11:47:27 +0100

    servers/manager: add gitlab runner config to manager

--- servers/manager-conf/manager.nix
+++ servers/manager-conf/manager.nix
@@ -1,10 +1,25 @@
 {
   manager = {config, pkgs, ...}:
   {
+    services.gitlab-runner.enable = true;
+    services.gitlab-runner.configText = ''
+concurrent = 1
+check_interval = 0
+
+[[runners]]
+  name = "manager"
+  url = "https://gitlab.com/ci"
+  token = "7d52ff93b9e0e24e371ab5edca4131"
+  executor = "shell"
+  [runners.cache]
+'';
     environment.systemPackages = [
       pkgs.nixops
-      pkgs.borgbackup
-      pkgs.ag
+      pkgs.gitlab-runner
     ];
+
+    users.extraUsers.gitlab-runner = {
+      shell = "/run/current-system/sw/bin/bash";
+    };
   };
 }
