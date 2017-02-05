b9cbb4468337d03312e18f9e768f1bee94904d64
Hendrik Schaeidt <he.schaeidt@gmail.com>
2017-02-05 13:11:12 +0100

    servers/manager: enable docker build daemon

--- servers/manager-conf/manager.nix
+++ servers/manager-conf/manager.nix
@@ -1,25 +1,43 @@
 {
   manager = {config, pkgs, ...}:
   {
-    services.gitlab-runner.enable = true;
-    services.gitlab-runner.configText = ''
-concurrent = 1
-check_interval = 0
+    services.gitlab-runner = {
+      enable = true;
+      configText = ''
+        concurrent = 1
+        check_interval = 0
 
-[[runners]]
-  name = "manager"
-  url = "https://gitlab.com/ci"
-  token = "7d52ff93b9e0e24e371ab5edca4131"
-  executor = "shell"
-  [runners.cache]
-'';
+        [[runners]]
+          name = "scaling-runner"
+          url = "https://gitlab.com/ci"
+          token = "7d52ff93b9e0e24e371ab5edca4131"
+          executor = "docker"
+          [runners.docker]
+            tls_verify = false
+            image = "ubuntu:latest"
+            privileged = false
+            disable_cache = false
+            volumes = ["/cache"]
+          [runners.cache]
+            Type = "s3"
+            ServerAddress = "http://127.0.0.1:9005"
+            AccessKey = "2XFZ4GUHYYO6M1UU4SFY"
+            SecretKey = "MToIJfOGVu3h6KGJB8Ohh8SnI1kQq+TJ2OgsknHy"
+            BucketName = "runner"
+            Insecure = true
+          [runners.machine]
+            IdleCount = 2
+            MachineDriver = "virtualbox"
+            MachineName = "auto-scale-runners-%s"
+            OffPeakIdleCount = 0
+            OffPeakIdleTime = 0
+        '';
+    };
+
     environment.systemPackages = [
       pkgs.nixops
-      pkgs.gitlab-runner
     ];
 
-    users.extraUsers.gitlab-runner = {
-      shell = "/run/current-system/sw/bin/bash";
-    };
+    virtualisation.docker.enable = true;
   };
 }
