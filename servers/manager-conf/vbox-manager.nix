4f08b4b1ca0fdba72bb1fe8420c6814f6096a3a1
Hendrik Schaeidt <he.schaeidt@gmail.com>
2017-02-07 00:20:31 +0100

    servers/manager: add proper shared folder mounting

--- servers/manager-conf/vbox-manager.nix
+++ servers/manager-conf/vbox-manager.nix
@@ -4,11 +4,24 @@
     { deployment.targetEnv = "virtualbox";
       deployment.virtualbox.memorySize = 1024; # MiB
       deployment.virtualbox.headless = true;
+
+      # shared folders
+      # http://nixos.org/nixops/manual/#opt-deployment.virtualbox.sharedFolders
+      # Note: the shared folder 'key' is equal the 'device' in fileSystems in the following declaration
       deployment.virtualbox.sharedFolders = {
-        home = {
-          hostPath = "/Users/hschaeidt";
-          readOnly = false;
+        Users = {
+          hostPath = "/Users";
+          readOnly = true;
         };
+      };
+
+      # mounting the above declared shared folder into the local file system
+      # https://www.virtualbox.org/manual/ch04.html#sf_mount_manual
+      # https://nixos.org/nixos/manual/#sec-instaling-virtualbox-guest
+      fileSystems."/Users" = {
+        fsType = "vboxsf";
+        device = "Users";
+        options = [ "rw" ];
       };
     };
 }
