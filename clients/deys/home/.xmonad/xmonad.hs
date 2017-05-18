7b13eb2e52bf7a132d301472b3f25498e11f5c05
Hendrik Schaeidt <hendrik@schaeidt.net>
2017-05-18 21:48:47 +0200

    clients/deys: xmonad spawns xmobar
    -  customize xmobarrc

--- clients/deys/home/.xmonad/xmonad.hs
+++ clients/deys/home/.xmonad/xmonad.hs
@@ -2,16 +2,22 @@ module Main (main) where
 
 import XMonad
 
-import Graphics.X11.Xlib
 import XMonad.Prompt.Pass
 import qualified Data.Map as M
+import XMonad.Util.Run
+import XMonad.Hooks.ManageDocks
 
 main :: IO ()
-main = xmonad $ def
-      { modMask = mod4Mask  -- super instead of alt (usually Windows key)
-      , terminal = "urxvt"
-      , keys = myKeys <+> keys def
-      }
+main = do
+  xmobar <- spawnPipe "xmobar ~/.config/xmobar/xmobarrc"
+  xmonad $ def
+    { modMask = mod4Mask  -- super instead of alt (usually Windows key)
+    , manageHook         = manageDocks <+> manageHook def
+    , layoutHook         = avoidStruts $ layoutHook def
+    , handleEventHook    = handleEventHook def <+> docksEventHook
+    , terminal = "urxvt"
+    , keys = myKeys <+> keys def
+    }
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList
     [ ((modm , xK_p)                              , passPrompt def)
