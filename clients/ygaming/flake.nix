7352eb4f28c23bb730505bf714b91c736275680d
Hendrik Schaeidt <he.schaeidt@gmail.com>
2022-05-02 21:46:08 +0000

    clients/ygaming: pin nixos-wsl to hschaeidt/nixos-wsl/21.11
    - pass extra args to modules and home-manager
    - fix styling in home.nix

--- clients/ygaming/flake.nix
+++ clients/ygaming/flake.nix
@@ -3,14 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
-    nixos-wsl.url = "github:hschaeidt/nixos-wsl";
+    nixos-wsl.url = "github:hschaeidt/nixos-wsl/21.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nixos-wsl, ... }: {
+  outputs = { self, nixpkgs, home-manager, nixos-wsl, ... }@attrs: {
     nixosConfigurations.ygaming = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = attrs;
       modules = [
         ./configuration.nix
         nixos-wsl.nixosModules.wsl
@@ -20,13 +21,14 @@
             automountPath = "/mnt";
             defaultUser = "nixos";
             startMenuLaunchers = true;
-            docker-desktop.enable = true;
+            docker.enable = true;
           };
         }
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = attrs;
           home-manager.users.nixos = import ./home.nix;
         }
       ];
--- clients/ygaming/home.nix
+++ clients/ygaming/home.nix
@@ -1,32 +1,37 @@
 {pkgs, ...}:
 {
-    imports = [];
+  imports = [];
 
-    home.packages = with pkgs; [
-        wget
-        firefox
+  home.packages = with pkgs; [
+    wget
+    firefox
+    aws-vault
+    amazon-ecr-credential-helper
+  ];
+
+  programs.vim = {
+    enable = true;
+    
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
     ];
 
-    programs.vim = {
-        enable = true;
-        plugins = with pkgs.vimPlugins; [
-        vim-nix
-        ];
-        settings = {
-        ignorecase = true;
-        smartcase = true;
-        number = true;
-        relativenumber = true;
-        };
+    settings = {
+      ignorecase = true;
+      smartcase = true;
+      number = true;
+      relativenumber = true;
     };
+  };
 
-    programs.git = {
-        enable = true;
-        userName = "Hendrik Schaeidt";
-        userEmail = "he.schaeidt@gmail.com";
-        extraConfig = {
-        credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe";
-        core.editor = "vim";
-        };
+  programs.git = {
+    enable = true;
+    userName = "Hendrik Schaeidt";
+    userEmail = "he.schaeidt@gmail.com";
+    extraConfig = {
+      credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe";
+      core.editor = "vim";
     };
+  };
+
 }
